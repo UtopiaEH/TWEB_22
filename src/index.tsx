0b398b9c6abbc0d0b3cf53d378fdcc80948c7513
UtopiaEH <utopiaeh01@gmail.com>
2022-01-28 16:53:47 +0200

    was initialized store

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { ReactElement } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import storeProvider from './mst/stores/StoreProvider'
+import reportWebVitals from './reportWebVitals'
 
+const StoreContext = React.createContext<any>(null)
+
+export const useStore = () => React.useContext(StoreContext)
+
+const StoreProvider = ({ children }: { children: ReactElement }) => {
+    return <StoreContext.Provider value={ storeProvider }>
+        { children }
+    </StoreContext.Provider>
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <StoreProvider>
+            <App />
+        </StoreProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
