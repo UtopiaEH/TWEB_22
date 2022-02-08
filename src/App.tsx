626cb909186adc5874837b83a027dadfe9ca8ae6
UtopiaEH <utopiaeh01@gmail.com>
2022-02-08 21:11:01 +0200

    basic tpl

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,18 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import { Layout } from 'antd'
+import { HeaderLayout } from './Layouts/HeaderLayout'
+import { ContentLayout } from './Layouts/ContentLayout'
+
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={ logo } className='App-logo' alt='logo' />
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Layout className='layout'>
+                <HeaderLayout />
+                <ContentLayout />
+            </Layout>
+        </>
     )
 }
 
---
+++ src/Layouts/CardCustom.tsx
@@ -0,0 +1,10 @@
+import Card from 'antd/lib/card'
+
+export const CardCustom = ({ title, description }: { title: string, description: string }) => {
+    return (
+        <Card title={ title } bordered={ false }>
+            { description }
+        </Card>
+    )
+
+}
