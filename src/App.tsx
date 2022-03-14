f2f45a0d14b0e9cf529bd08ad02a54507163c6c6
UtopiaEH <constantin@avans.no>
2022-03-14 20:20:26 +0200

    added routes test

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import './App.css'
 import { Layout } from 'antd'
 import { HeaderLayout } from './Layouts/HeaderLayout'
 import { ContentLayout } from './Layouts/ContentLayout'
+import { Routes, Route } from 'react-router-dom'
+import { ROUTS } from './Roouts'
 
 function App() {
     return (
         <>
             <Layout className='layout'>
                 <HeaderLayout />
-                <ContentLayout />
+                <Routes>
+                    <Route path={ ROUTS[0].path } element={ <ContentLayout /> } />
+                </Routes>
+                {/*<ContentLayout />*/ }
             </Layout>
         </>
     )
---
+++ src/Roouts.ts
@@ -0,0 +1,17 @@
+export const ROUTS = [
+    {
+        id: 1,
+        path: '/home',
+        route_name: 'Home'
+    },
+    {
+        id: 2,
+        path: '/login',
+        route_name: 'Login'
+    },
+    {
+        id: 3,
+        path: '/articles',
+        route_name: 'Articles'
+    }
+]
